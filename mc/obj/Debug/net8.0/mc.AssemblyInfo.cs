4cf0b875c37c6264686ded5f0b93990a7eec5c69
BHAVYA MITTAL <103727337+MITTALBHAVYA@users.noreply.github.com>
2023-12-15 19:50:53 +0530

    mcassembly

--- mc/obj/Debug/net8.0/mc.AssemblyInfo.cs
+++ mc/obj/Debug/net8.0/mc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc9e5587c780533e1d8db3283ae64babf4ed5e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("mc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
